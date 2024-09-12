3f1cd199f205882c5cc90f4c144292b8a4ebe31e
webappstudioz <bhartirani7902@gmail.com>
2024-09-12 23:40:43 +0530

    Worked on bug fixing

--- app/Http/Controllers/NumberWinningController.php
+++ app/Http/Controllers/NumberWinningController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\NumberWinning;
 use App\Traits\SendResponseTrait;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -22,20 +23,33 @@ class NumberWinningController extends Controller
             
             $validationRules = [
                 'numbers'   => 'required', 
-                'time'      => 'required'
             ];
             
             $validator = Validator::make($request->all(), $validationRules);
             if ($validator->fails()) { 
                 return $this->apiResponse('error', '422', $validator->errors()->first());
             } 
-             
+           
+            $timeZones = [
+                ['start' => '00:00', 'end' => '11:00'],
+                ['start' => '11:00', 'end' => '15:00'],
+                ['start' => '15:00', 'end' => '18:00'],
+                ['start' => '18:00', 'end' => '21:00'],
+            ];
+
+            $currentTime = Carbon::now()->format('H:i');
+            foreach ($timeZones as $index => $range) {
+                if ($currentTime >= $range['start'] && $currentTime < $range['end']) {
+                   $zone_id = $index+1;
+                }
+            }
+
             foreach($request->numbers as $value){
                 NumberWinning::create([
                     'user_id'       => authId(),
                     'amount'        => $value->amount,
                     'w_number'      => $value->w_number,
-                    'timezone'      => 1,
+                    'timezone'      => $zone_id,
                 ]);
             }
              return $this->apiResponse('success', '200', 'Number '. config('constants.SUCESS.ADD_DONE')); 
